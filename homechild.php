1b7221d3099d234459c437bc39ba585c6ad8038e
MarioIbrahem1 <9089mario2003@gmail.com>
2023-05-27 00:57:29 +0300

    upload the design

---
+++ homechild.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <link rel="stylesheet" href="homechild.css">
+    <link rel="website icon" type="png" href="images/Mother & Baby.png" style="border-radius: 100%" />
+    <title>
+      Child Care
+    </title>
+  </head>
+
+  <body>
+    <div class="cards-list">
+
+      <div class="card">
+
+        <div class="card_image">
+
+          <image src="images/communication.jpg" />
+        </div>
+
+        <div class="card_title">
+          <form target="_blank" action="communication.php"><button id="card1">Communication</button></form>
+        </div>
+      </div>
+
+
+      <div class="card">
+
+        <div class="card_image">
+
+          <image src="images/Physical Health.jpg" />
+        </div>
+
+        <div class="card_title">
+          <form target="_blank" action="physical.php"> <button id="card2">Physical-Health</button></form>
+        </div>
+      </div>
+
+      <div class="card">
+
+        <div class="card_image">
+
+          <image src="images/Psychological Health.jpg" />
+        </div>
+
+        <div class="card_title">
+          <form target="_blank" action="Psyoo.php"> <button id="card3">Mental-Health</button></form>
+        </div>
+      </div>
+
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <form target="_blank" action="http://localhost/Org/getstarted.php?    "><button class="btn1">back</button></form>
+            </li>
+            <li>
+              <form target="_blank" action="http://localhost/Org/Home.php"><button class="btn2">Home</button></form>
+            </li>
+            <li>
+              <form target="_blank" action="http://localhost/Org/login.php"><button class="btn3">Log In</button></form>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <pre class="title">
+            <B> Child  Care</B>
+          </pre>
+
+      <div class="logo">
+        <img class="logo_img" src="images/Mother__Baby.png">
+      </div>
+
+
+  </body>
+
+</html>
